e6e023c3adda5df2d1efd6ad8e7e0e6b123af575
Joseph Michael Matembu <jmatembu@gmail.com>
2016-09-16 23:31:07 +0300

    Add data model

---
+++ assets/js/app.js
@@ -0,0 +1,328 @@
+var entireData = {
+	app: {
+		appName: "Encylopedia. Modern Developer",
+		appImage: "assets/images/encyclopedia.jpg"
+	},
+	profile: {
+		name: "Paul Irish",
+		description: "American front-end engineer and a developer advocate for Google Chrome, the most popular web browser in the world by market share. He is widely recognized as a thought leader and a leading evangelist in web technologies, including JavaScript and CSS.He was named Developer of the Year by The Net Awards in 2014.",
+		currentProject: {
+			name: "Aurgasm",
+			url: "#"
+		},
+		slides: [
+			{
+				source: "assets/images/paul.jpg",
+				altText: ""
+			},
+			{
+				source: "assets/images/paul.jpg",
+				altText: ""
+			},
+			{
+				source: "assets/images/paul.jpg",
+				altText: ""
+			}
+		],
+		profileLinks: [
+			{
+				text: "Silicon Valley, CA",
+				url: "#",
+				type: "location"
+			},
+			{
+				text: "@paul_irish",
+				url: "https://twitter.com/paul_irish",
+				type: "twitter"
+			},
+			{
+				text: "paulirish",
+				url: "https://github.com/paulirish",
+				type: "github"
+			},
+			{
+				text: "Homepage",
+				url: "#",
+				type: "website"
+			},
+			{
+				text: "Blog",
+				url: "",
+				type: "blog"
+			},
+			{
+				text: "Company",
+				url: "https://plus.google.com/+GoogleChromeDevelopers/posts",
+				type: "company"
+			}
+		],
+		contribution: {
+			title: "Noteworthy Contributions to Humanity",
+			list: [
+				{
+					name: "Open Source Community",
+					description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum assumenda dolorem modi, ipsam nam, eveniet optio est laborum, fuga voluptatibus illo consequuntur.",
+					image: {
+						path: "assets/images/placeholder.jpg",
+						altText: ""
+					},
+					links: [
+						{
+							text: "paulirish.com",
+							url: "http://www.paulirish.com/",
+							type: "website"
+						},
+						{
+							text: "@paul_irish",
+							url: "https://twitter.com/paul_irish?lang=en",
+							type: "twitter"
+						},
+						{
+							text: "paulirish",
+							url: "https://github.com/paulirish",
+							type: "github"
+						}
+					]
+				},
+				{
+					name: "jQuery Community",
+					description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum assumenda dolorem modi, ipsam nam, eveniet optio est laborum, fuga voluptatibus illo consequuntur.",
+					image: {
+						path: "assets/images/placeholder.jpg",
+						altText: ""
+					},
+					links: [
+						{
+							text: "paulirish.com",
+							url: "http://www.paulirish.com/",
+							type: "website"
+						},
+						{
+							text: "@paul_irish",
+							url: "https://twitter.com/paul_irish?lang=en",
+							type: "twitter"
+						},
+						{
+							text: "paulirish",
+							url: "https://github.com/paulirish",
+							type: "github"
+						}
+					]
+				}
+			]
+		},
+		gratitude: {
+			title: "You Have Likely Benefited from Paul's Work and Altruism",
+			description: "How to show your gratitude:",
+			methods: [
+				{
+					title: "Favourite his projects on GitHub",
+					action: {
+						text: "Favourite",
+						url: "#"
+					},
+					link: {
+						text: "paulirish",
+						url: "https://github.com/paulirish"
+					}
+				},
+				{
+					title: "Tip Paul",
+					action: {
+						text: "Send it Now",
+						url: "#"
+					},
+					link: {
+						text: "Gratipay",
+						url: "#"
+					}
+				},
+				{
+					title: "Thank Paul on Twitter or via email",
+					action: {
+						text: "@paul_irish",
+						url: "https://twitter.com/paul_irish?lang=en"
+					},
+					link: {
+						text: "paul@gmail.com",
+						url: "mailto:paul@gmail.com"
+					}
+				}
+			]
+		},
+		biography: {
+			title: "Biography",
+			description: "<p>Hi folks. I'm Paul. I'm a front-end developer who loves the web. I'm always thinking a lot about how to make you more productive with tools that your workflow, help you make slicker, sexier, mobile web sites and webapps.</p><p>I work on the <a href='#'>Google Chrome</a> team focused on Developer Tooling. Chrome DevTools and everything else Chrome can do to make you a productive web developer.</p><p>I've developed tools like <a href='#'>Modernizr</a>, <a href='#'>Yeoman</a>, CSS3 Please and HTML5 Boilerplate, curated HTML5 Rocks, and co-created projects like HTML5 Please, mothereffingtextshadow, mothereffinghsl, Move the Web Forward, Mother-effing Animated GIF, W3Fools, <a href='#'>chromestatus.com</a>, HTML5 Readiness, Font-end Coding Standards, Typee Rendering Project.</p>"
+		},
+		trivia: {
+			title: "Trivia",
+			description: "<p>I used to host the amazing yayQuery Podcast. I've long loved music and kept the <a href='#'>Aurgasm</a> music blog up for 10 years.</p>"
+		},
+		work: {
+			title: "Work History",
+			history: [
+				{
+					company: "Developer Advocate, TPM for Google Chrome",
+					period: "June 1010 - Present"
+				},
+				{
+					company: "jQuery Community",
+					period: "January 2008 - Present"
+				},
+				{
+					company: "Senior Consultant, Emerging Interactions",
+					period: "Octover 2006 - April 2010"
+				},
+				{
+					company: "eInvite Ecommerce Manager",
+					period: "June 2004 - July 2006"
+				},
+				{
+					company: "Writing Tutor",
+					period: "August 2003 - May 2004"
+				}
+			] 
+		},
+		skills: {
+			title: "Skills",
+			description: "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p><p>Praesentium error eum alias sit fugiat explicabo cum ipsa beatae possimus.</p>",
+			skillSet: [
+				{
+					name: "Frontend",
+					scale: 70
+				},
+				{
+					name: "Backend",
+					scale: 30
+				}
+			],
+			graphs: [
+				{
+					name: "Frontend Development",
+					yAxisLabels: ["Expert", "Proficient", "Competent", "Learning"],
+					values: [100, 80, 100, 35, 20],
+					xAxisLabels: ["JavaScript", "HTML/CSS", "ReactJS", "AngularJS", "jQuery"]
+				},
+				{
+					name: "Backend Development",
+					yAxisLabels: ["Expert", "Proficient", "Competent", "Learning"],
+					values: [100, 80, 100, 35, 20],
+					xAxisLabels: ["JavaScript", "HTML/CSS", "ReactJS", "AngularJS", "jQuery"]
+				},
+				{
+					name: "Other Skills",
+					yAxisLabels: ["Expert", "Proficient", "Competent", "Learning"],
+					values: [100, 80, 100, 35, 20],
+					xAxisLabels: ["Written Communication", "Problem Solving", "Project Management", "Research", "Public Speaking"]
+				}
+			]
+		},
+		twitter: {
+			title: "Twitter",
+			author: "Paul Irish",
+			handle: "@paul_irish",
+			tweets: [
+				{
+					author: "Paul Irish",
+					handle: "@paul_irish",
+					content: "<a href='#'>@alexlande</a> (srsly: looks really good! well done. (love that show so hard))",
+					posted: "8:37 - 29 May 2016",
+				},
+				{
+					author: "Paul Irish",
+					handle: "@paul_irish",
+					content: "<a href='#'>@alexlande</a> (srsly: looks really good! well done. (love that show so hard))",
+					posted: "8:37 - 29 May 2016",
+				},
+				{
+					author: "Paul Irish",
+					handle: "@paul_irish",
+					content: "<a href='#'>@alexlande</a> (srsly: looks really good! well done. (love that show so hard))",
+					posted: "8:37 - 29 May 2016",
+				},
+				{
+					author: "Paul Irish",
+					handle: "@paul_irish",
+					content: "<a href='#'>@alexlande</a> (srsly: looks really good! well done. (love that show so hard))",
+					posted: "8:37 - 29 May 2016",
+				},
+				{
+					author: "Paul Irish",
+					handle: "@paul_irish",
+					content: "<a href='#'>@alexlande</a> (srsly: looks really good! well done. (love that show so hard))",
+					posted: "8:37 - 29 May 2016",
+				}
+			],
+			link: {
+				text: "Load More",
+				url: "#"
+			}
+		},
+		articles: {
+			title: "Articles",
+			posts: [
+				{
+					title: "WebKit for Developers",
+					published: "Mar 25, 2016",
+					source: "paulirish.com"
+				},
+				{
+					title: "Advanced Performance Audits With DevTools",
+					published: "Feb 12, 2016",
+					source: "paulirish.com"
+				},
+				{
+					title: "What's shared in all WebKit browsers",
+					published: "Jan 25, 2016",
+					source: "paulirish.com"
+				}
+			],
+			link: {
+				text: "Load More",
+				url: "#"
+			}
+		},
+		comments: {
+			title: "Comments and Answers",
+			posts: [
+				{
+					comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum sit voluptatibus fugit ad placeat maiores mollitia. Nesciunt ad ea est deserunt!",
+					published: "a day ago",
+					source: "paulirish.com"
+				},
+				{
+					comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum sit voluptatibus fugit ad placeat maiores mollitia. Nesciunt ad ea est deserunt!",
+					published: "a day ago",
+					source: "paulirish.com"
+				},
+				{
+					comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum sit voluptatibus fugit ad placeat maiores mollitia. Nesciunt ad ea est deserunt!",
+					published: "a day ago",
+					source: "paulirish.com"
+				},
+				{
+					comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum sit voluptatibus fugit ad placeat maiores mollitia. Nesciunt ad ea est deserunt!",
+					published: "a day ago",
+					source: "paulirish.com"
+				}
+			],
+			link: {
+				text: "Load More",
+				url: "#"
+			}
+		}
+	},
+	forms: [
+		{
+			name: "formSearch",
+			fields: [
+				{
+					name: "searchText",
+					type: "search",
+					placeholder: "Search...",
+					class: "searchForm"
+				}
+			]
+		}
+	],
+};
